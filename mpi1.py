e6cb00326f535caeb15cc9bbd729d20f4dc4f83b
propellermint <seandrue@yandex.ru>
2020-06-14 19:14:27 +0300

    ver.1.0

---
+++ mpi1.py
@@ -0,0 +1,29 @@
+import numpy as np
+import pandas as pd
+from mpi4py import MPI
+
+
+size = 1000
+pd.DataFrame(np.random.random(size=size),columns=['x']).to_csv('x0.csv')
+pd.DataFrame(np.random.random(size=size),columns=['x']).to_csv('x1.csv')
+
+#Считывание таблиц
+data0 = pd.read_csv("x0.csv")
+data1 = pd.read_csv("x1.csv")
+
+
+
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+
+if rank == 1:
+    data = pd.read_csv('x1.csv')
+    res = data.x.sum()
+    comm.send(res, dest=0)
+elif rank == 0:
+    data = pd.read_csv('x0.csv')
+    x_0 = data.x.sum()
+    x_1 = comm.recv(source=1)
+    x1 = data0.x.sum()
+    x2 = data1.x.sum()
+    print("Сумма всех элементов X = ", x1 + x2)
